a77341d37fa52801e3956ebdabc9d97909dcf440
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-26 21:48:35 -0300

    Create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
+import { GlobalStyles } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <h1>Hello</h1>
     </div>
   );
