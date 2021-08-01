c8b5f108171a32228c5142a1aebb3ab1ef0950aa
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-31 22:40:40 -0300

    Insert table line transaction

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -25,7 +25,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
@@ -38,9 +38,18 @@ export function TransactionsProvider({ children }: TransactionsProviderProps){
       .then(response => setTransactions(response.data.transactions));
   }, [])
 
-  function createTransaction(transaction: TransactionInput){
+  async function createTransaction(transactionInput: TransactionInput){
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
 
-    api.post('/transactions', transaction)
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ])
   }
 
   return (
