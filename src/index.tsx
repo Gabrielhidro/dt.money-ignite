6272b2379466ef1cb3336862a6cc01338ba87ed9
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-31 23:52:19 -0300

    Clean static values

--- src/index.tsx
+++ src/index.tsx
@@ -8,28 +8,7 @@ createServer({
     transaction: Model,
   },
 
-  seeds(server){
-    server.db.loadData({
-      transactions: [
-      {
-        id: 1,
-        title: 'Freelancer',
-        type: 'deposit',
-        category: 'DEV',
-        amount: 5000,
-        createdAt: new Date('2021-02-12 09:08:00')
-      },
-      {
-        id: 2,
-        title: 'Aluguel',
-        type: 'withdraw',
-        category: 'Casa',
-        amount: 980,
-        createdAt: new Date('2021-02-02 09:08:00')
-      }
-    ],
-    })
-  },
+
 
   routes(){
     this.namespace = 'api';
